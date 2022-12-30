c2dbe5ac3a9f29abfbc8514b22139be3a11e7e96
Gabriel Loutfalla Kanawati <gabrielkanawati3@gmail.com>
2022-12-30 11:30:15 -0300

    Configuração Storybook e Criação de Componentes

--- src/components/Heading.stories.tsx
+++ src/components/Heading.stories.tsx
@@ -1,9 +1,9 @@
-import { Text, TextProps } from './Text';
+import { Heading, HeadingProps } from './Heading';
 import { Meta, StoryObj } from '@storybook/react';
 
 export default {
-  title: 'Components/Text',
-  component: Text,
+  title: 'Components/Heading',
+  component: Heading,
   args: {
     children: 'Lorem ipsum',
     size: 'md',
@@ -16,29 +16,34 @@ export default {
       },
     },
   },
-} as Meta<TextProps>;
+} as Meta<HeadingProps>;
 
-export const Default: StoryObj<TextProps> = {};
+export const Default: StoryObj<HeadingProps> = {};
 
-export const Small: StoryObj<TextProps> = {
+export const Small: StoryObj<HeadingProps> = {
   args: {
     size: 'sm',
   },
 };
 
-export const Large: StoryObj<TextProps> = {
+export const Large: StoryObj<HeadingProps> = {
   args: {
     size: 'lg',
   },
 };
 
-export const CustomComponent: StoryObj<TextProps> = {
+export const CustomComponent: StoryObj<HeadingProps> = {
   args: {
     asChild: true,
-    children: <p>Test with p tag</p>,
+    children: <h1>Heading with h1</h1>,
   },
   argTypes: {
     children: {
+      table: {
+        disable: true,
+      },
+    },
+    asChild: {
       table: {
         disable: true,
       },
---
+++ src/components/TextInput.tsx
@@ -0,0 +1,46 @@
+import { InputHTMLAttributes, ReactNode } from 'react';
+import { Slot } from '@radix-ui/react-slot';
+
+export interface TextInputRootProps {
+  children: ReactNode;
+}
+
+function TextInputRoot(props: TextInputRootProps) {
+  return (
+    <div className='flex items-center gap-3 py-4 px-3 h-12 rounded bg-grey-800 w-full focus-within:ring-2 ring-cyan-300'>
+      {props.children}
+    </div>
+  );
+}
+
+TextInputRoot.displayName = 'TextInput.Root';
+
+export interface TextInputIconProps {
+  children: ReactNode;
+}
+
+function TextInputIcon(props: TextInputIconProps) {
+  return <Slot className='w-6 h-6 text-grey-400'>{props.children}</Slot>;
+}
+
+TextInputIcon.displayName = 'TextInput.Icon';
+
+export interface TextInputInputProps
+  extends InputHTMLAttributes<HTMLInputElement> {}
+
+function TextInputInput(props: TextInputInputProps) {
+  return (
+    <input
+      {...props}
+      className='bg-transparent outline-none flex-1 text-grey-100 text-xs placeholder:text-grey-400'
+    />
+  );
+}
+
+TextInputInput.displayName = 'TextInput.Input';
+
+export const TextInput = {
+  Root: TextInputRoot,
+  Input: TextInputInput,
+  Icon: TextInputIcon,
+};
